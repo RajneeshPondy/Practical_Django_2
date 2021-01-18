6ff246543a8669631f33234f4b9de85c33d2d848
RajneeshPython <rajneeshkmr668@gmail.com>
2021-01-18 16:47:04 +0530

    changes in settings

--- booktime/settings.py
+++ booktime/settings.py
@@ -123,11 +123,11 @@ MEDIA_URL = '/media/'
 # Email Configurations
 if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST_USER = "rajneeshpython@gmail.com"
+    EMAIL_HOST_USER = "youremail"
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_PORT = 587
     EMAIL_USE_TLS = True
-    EMAIL_HOST_PASSWORD = "python123"
+    EMAIL_HOST_PASSWORD = "youremailpass"
 else:
     EMAIL_BACKEND = (
     "django.core.mail.backends.console.EmailBackend"
