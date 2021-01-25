afdff247c4f157f424eb83e88e2397b80088d535
RajneeshPython <rajneeshkmr668@gmail.com>
2021-01-26 00:24:23 +0530

    Added Product List and Detail functionalities and management cmd to import data to db

--- main/urls.py
+++ main/urls.py
@@ -1,18 +1,32 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from django.views.generic.detail import DetailView
+
 from main import views
+from main import models
 
 # app_name = 'main'
 
 urlpatterns = [
-    path("contact-us/",
-         views.ContactUsView.as_view(),
-         name="contact_us",
-         ),
+    path(
+        "contact-us/",
+        views.ContactUsView.as_view(),
+        name="contact_us",
+    ),
     path(
         "about-us/",
-        TemplateView.as_view(template_name="about_us.html"), name='about_us'),
+        TemplateView.as_view(template_name="about_us.html"),
+        name="about_us",
+    ),
+    path("", TemplateView.as_view(template_name="home.html")),
     path(
-        "",
-        TemplateView.as_view(template_name="home.html")),
+        "products/<slug:tag>/",
+        views.ProductListView.as_view(),
+        name="products",
+    ),
+    path(
+        "product/<slug:slug>/",
+        DetailView.as_view(model=models.Product),
+        name="product",
+    ),
 ]
